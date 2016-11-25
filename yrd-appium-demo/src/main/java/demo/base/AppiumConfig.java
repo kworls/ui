a767887eb26be227de806d71a3e2e80322565416
fengzhao13 <fengzhao13@creditease.cn>
2016-11-25 18:30:06 +0800

    Initial commit

---
+++ yrd-appium-demo/src/main/java/demo/base/AppiumConfig.java
@@ -0,0 +1,51 @@
+package main.java.demo.base;
+
+/**
+ * Created by Administrator on 2016/11/22.
+ */
+public class AppiumConfig{
+
+    public String CURRENT_BANK;
+    public String DEVICE_NAME;
+    public String APPIUM_PORT;
+    public String PLANTFORM_VERSION;
+
+
+    public static AppiumConfig custom(){
+
+        return new AppiumConfig();
+    }
+
+    private AppiumConfig(){}
+
+    protected void setCURRENT_BANK(String CURRENT_BANK ){
+        this.CURRENT_BANK=CURRENT_BANK;
+    }
+    public String getCURRENT_BANK(){
+        return this.CURRENT_BANK;
+    }
+
+    protected void setDEVICE_NAME(String DEVICE_NAME ){
+        this.DEVICE_NAME=DEVICE_NAME;
+    }
+    public String getDEVICE_NAME(){
+        return this.DEVICE_NAME;
+    }
+
+    protected void setAPPIUM_PORT(String APPIUM_PORT ){
+        this.APPIUM_PORT=APPIUM_PORT;
+    }
+    public String getAPPIUM_PORT(){
+        return this.APPIUM_PORT;
+    }
+
+    protected void setPLANTFORM_VERSION(String PLANTFORM_VERSION ){
+        this.PLANTFORM_VERSION=PLANTFORM_VERSION;
+    }
+    public String getPLANTFORM_VERSION(){
+        return this.PLANTFORM_VERSION;
+    }
+
+
+
+}
